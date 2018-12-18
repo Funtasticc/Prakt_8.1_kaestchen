2ce9f5130443c24ec90c209133f381788ba5cf4e
Steffen <steffen.moeller1993@gmx.net>
2018-12-18 06:36:23 +0100

    Update regnende_kaestchen.cpp

--- regnende_kaestchen.cpp
+++ regnende_kaestchen.cpp
@@ -6,30 +6,33 @@ using namespace std;
 
 struct Box
 {
-	int x;
-	int y;
-	int delta_y;
+	int x; // x-Koordinate (Zeile)
+	int y; // y-Koordinate (Spalte)
+	int delta_y; // aktuelle Fallgeschwindigkeit des Kaestchens
 };
 
-const int box_max = 10, box_size = 40;
-const int difficulty = 5; //Sleeptimer
-//const int box_size = 50;
-//const int box_max = gip_win_sizeX / (box_size + 20);
-const int box_max_delta_y = 90; // aktuelle Fallgeschwindigkeit dieses Kaestchens
+const int box_max = 10 /*maximale Anzahl von Boxen*/, box_size = 40; // Box groesse
+
 
 // draw_boxes():
 // Die Anzahl der Kaestchen wird nicht als zweiter Parameter uebergeben,
 // da diese Anzahl als globale Konstante box_max im gesamten Programm bekannt ist ...
 
-void prepare_box_at_top(Box &b, int count)
+// Die "+10" bewirken, dass alle Kaestchen leicht nach rechts versetzt werden
+// und somit links vom linksten Kaestchen noch etwas Platz bleibt (dieses
+// also nicht am Rand klebt).
+// Die "+20" sorgen fuer einen Zwischenraum von 20 zwischen den Kaestchen ...
+
+void prepare_box_at_top(Box &b, int i)
 {
-	b.x = count * (box_size + 20) + 10;
+	b.x = i * (box_size + 20) + 10; // +10 = leicht nach rechts, +20 = zwischenraum
 	b.y = 0;
 
-	if (b.delta_y > 0) b.delta_y += 10;
+	if (b.delta_y > 0) 
+		b.delta_y += 10; // wenn die Startgeschwindigkeit gesetzt ist über gip_zufall, wird sie jedes Mal um 10 erhöht wenn ein Kästchen angeklickt wird
 	else
 	{
-		b.delta_y = gip_zufall(20, 30/*box_max_delta_y / 2*/); // Startgeschwindigkeit ist ganzzahlige Zufallszahl zwischen 10 und 30 ...
+		b.delta_y = gip_zufall(10, 30); // Startgeschwindigkeit ist ganzzahlige Zufallszahl zwischen 10 und 30 ...
 	}
 }
 
@@ -67,13 +70,15 @@ bool update_boxes(Box boxes[])
 	for (int i = 0; i < box_max; i++)
 	{
 		boxes[i].y += boxes[i].delta_y;
-		if (boxes[i].y > gip_win_size_y/*gip_win_sizeY*/) windowEnd = true;
+		if (boxes[i].y > gip_win_size_y) // SOLANGE die Boxen oberhalb vom unteren Fenster Ende sind, ist Window End auf true
+			windowEnd = true;
 
 	}
-	if (windowEnd) return false;
+	if (windowEnd)
+		return false; // wenn eine Box das Ende erreicht, wird windowEnd auf false gesetzt das Programm beendet
 	else
 	{
-		draw_boxes(boxes);
+		draw_boxes(boxes); // ANSONSTEN werden weiter neue Boxen gezeichnet
 		return true;
 	}
 }
@@ -88,14 +93,13 @@ bool update_boxes(Box boxes[])
 // * Erhoehe die Fallgeschwindigkeit des Kaestchens um 10
 // * Setze das Kaestchen an den oberen Rand
 
-void check_if_box_clicked(Box boxes[])
+void handle_mouse_click(Box boxes[])
 {
-	int mx = gip_mouse_x();
-	int my = gip_mouse_y();
-
 	for (int i = 0; i < box_max; i++)
 	{
-		if (mx >= boxes[i].x && mx <= boxes[i].x + box_size && my >= boxes[i].y && my <= boxes[i].y + box_size) prepare_box_at_top(boxes[i], i);
+		if (gip_mouse_x() >= boxes[i].x && gip_mouse_x() <= boxes[i].x + box_size && 
+			gip_mouse_y() >= boxes[i].y && gip_mouse_y() <= boxes[i].y + box_size) 
+			prepare_box_at_top(boxes[i], i);
 	}
 }
 
@@ -107,16 +111,17 @@ int main()
 // Die "+20" sorgen fuer einen Zwischenraum von 20 zwischen den Kaestchen ...
 	Box boxes[box_max] = { 0 };
 	bool keep_going = true;
-	for (int i = 0; i < box_max; i++)
+	for (int i = 0; i < box_max; i++) {
 		prepare_box_at_top(boxes[i], i);
-	draw_boxes(boxes);
+		draw_boxes(boxes);
+	}
 	// Startgeschwindigkeit ist ganzzahlige Zufallszahl zwischen 10 und 30 ...
 	while (keep_going && gip_window_not_closed())
 	{
 		for (int loop_count = 0; loop_count < 200; loop_count++) {
-			gip_wait(difficulty); // warte 5 Milli-Sekunden
+			gip_wait(5); // warte 5 Milli-Sekunden
 			if (gip_mouse_button1_pressed())
-				check_if_box_clicked(boxes);
+				handle_mouse_click(boxes);
 		}
 		keep_going = update_boxes(boxes);
 		// Berechne die neue Fall-Position aller Kaestchen und pruefe,
